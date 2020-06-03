dc1c08a71f8eea8cfdee8dc7fde3d0653ccb4e7a
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-06-03 00:56:47 -0600

    far from being finished here but i need to get to sleep since its 1am, i will revisit this tomorrow after lecture

--- js/index.js
+++ js/index.js
@@ -1,1 +1,57 @@
 // Your code goes here
+
+/* PSEUDO CODE */
+// the element -> the event -> the code we want to run
+
+
+// mouseover
+const newLogo = document.querySelector('.logo-heading');
+
+newLogo.addEventListener('mouseenter', (event)=>{
+    event.target.style.color = 'hotpink';
+    event.target.textContent = 'Funk 🅱️us';
+});
+
+const oldLogo = document.querySelector('.logo-heading');
+oldLogo.addEventListener('mouseleave', (event)=>{
+    event.target.style.color = 'black';
+    event.target.textContent = 'Funky Busssss';
+});
+// keydown
+
+// wheel
+const zoomImg = document.querySelectorAll('img');
+
+console.log(zoomImg);
+
+// drag & drop
+
+// click event
+const pinkLinks = document.querySelectorAll('nav a').forEach(el => {
+    el.addEventListener('click', (event)=>{
+        el.style.color = 'hotpink';
+    });
+    el.addEventListener('dblclick', (event)=>{
+        el.style.color = 'limegreen';
+    })
+});
+// load
+
+// focus
+
+// resize
+
+// scroll
+
+// select
+
+// dblclick
+const schoolBus = document.querySelector('img');
+schoolBus.addEventListener('dblclick', ()=>{
+    schoolBus.src='https://image.shutterstock.com/image-photo/yellow-bus-isolated-on-white-260nw-17008966.jpg';
+});
+
+// Nest two similar events somewhere in the site and prevent the event propagation properly
+const bubbleButtons = document.getElementsByClassName('btn');
+
+//  Stop the navigation items from refreshing the page by using preventDefault()
